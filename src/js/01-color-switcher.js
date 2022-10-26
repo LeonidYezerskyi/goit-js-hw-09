9f2b37377259af133e3dbf4688a0dc1b5d4c4afc
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-26 17:32:36 +0300

    Виконує завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+let timerId;
+
+btnStart.addEventListener('click', onClick);
+
+function changeColor() {
+    let randomCoolor = getRandomHexColor();
+    body.style.backgroundColor = randomCoolor;
+}
+
+function onClick(event) {
+    changeColor();
+    timerId = setInterval(changeColor, 1000);
+};
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+});
