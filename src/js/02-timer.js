d3362fbd74dd216d2480eb2534127a7682b6e493
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-26 22:12:37 +0300

    Виконує завдання 1

---
+++ src/js/02-timer.js
@@ -0,0 +1,42 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const input = document.querySelector('#datetime-picker');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        const currentDate = new Date();
+        const difference = selectedDates[0] - currentDate;
+        if (selectedDates[0] < currentDate) {
+            window.alert("Please choose a date in the future");
+        }
+        console.log(difference);
+        console.log(selectedDates);
+        console.log(convertMs(difference))
+  },
+};
+
+flatpickr(input, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
