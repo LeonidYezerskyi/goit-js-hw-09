0ca8f465b8eef83456b8437f6d4857b7ee4fbf6a
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-27 15:56:38 +0300

    Вносить зміни в завдання 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,14 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const input = document.querySelector('#datetime-picker');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+const btnStart = document.querySelector('[data-start]');
+
+btnStart.setAttribute('disabled', true);
+let timerId;
 
 const options = {
   enableTime: true,
@@ -9,34 +17,57 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
     onClose(selectedDates) {
-        const currentDate = new Date();
-        const difference = selectedDates[0] - currentDate;
-        if (selectedDates[0] < currentDate) {
-            window.alert("Please choose a date in the future");
-        }
-        console.log(difference);
-        console.log(selectedDates);
-        console.log(convertMs(difference))
+      const currentDate = new Date();
+       
+      if (selectedDates[0] < currentDate) {
+  btnStart.disabled = true;
+            return window.alert("Please choose a date in the future");
+        } else {
+          btnStart.disabled = false;
+      }
+
   },
 };
 
-flatpickr(input, options);
+const flp = flatpickr(input, options);
+console.log(flp);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+};
+
+btnStart.addEventListener('click', btnOnClick);
+
+// function changeTime(flatpickr) {
+//   let date = new Flatpickr;
+//   const seconds = date.getSeconds();
+//   const transformedSeconds = seconds.toString().padStart(2, "0");
+
+// }
+
+
+function changeTime() {
+  const currentDate = new Date();
+  const difference = flp.selectedDates[0] - currentDate;
+  convertMs(difference);
+      console.log(convertMs(difference));
+  
 }
+
+function btnOnClick(event) {
+    changeTime();
+  timerId = setInterval(changeTime, 1000);
+  btnStart.disabled = true;
+  input.disabled = true;
+
+
+};
