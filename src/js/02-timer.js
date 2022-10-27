0b63fb096a951930362c96bb912e76a0d89a0390
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-27 22:17:42 +0300

    Фінальній правки в завданні 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,8 +7,25 @@ const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
 const btnStart = document.querySelector('[data-start]');
+const timerEl = document.querySelector('.timer')
 
-btnStart.setAttribute('disabled', true);
+timerEl.style.fontFamily = "Arial";
+timerEl.style.display = "flex";
+timerEl.style.gap = "5px";
+timerEl.style.justifyContent = "center";
+timerEl.style.marginTop = "20px";
+timerEl.style.color = "white";
+timerEl.style.backgroundColor = "green";
+
+btnStart.style.backgroundColor = "red";
+btnStart.style.color = "white";
+btnStart.style.borderColor = "black";
+
+input.style.borderColor = "red";
+input.style.backgroundColor = "black";
+input.style.color = "green";
+
+btnStart.disabled = true;
 let timerId;
 
 const options = {
@@ -20,12 +37,11 @@ const options = {
       const currentDate = new Date();
        
       if (selectedDates[0] < currentDate) {
-  btnStart.disabled = true;
-            return window.alert("Please choose a date in the future");
+        btnStart.disabled = true;
+        return window.alert("Please choose a date in the future");
         } else {
           btnStart.disabled = false;
       }
-
   },
 };
 
@@ -41,33 +57,31 @@ function convertMs(ms) {
   const hours = Math.floor((ms % day) / hour);
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 };
 
 btnStart.addEventListener('click', btnOnClick);
 
-// function changeTime(flatpickr) {
-//   let date = new Flatpickr;
-//   const seconds = date.getSeconds();
-//   const transformedSeconds = seconds.toString().padStart(2, "0");
-
-// }
-
-
-function changeTime() {
-  const currentDate = new Date();
-  const difference = flp.selectedDates[0] - currentDate;
-  convertMs(difference);
-      console.log(convertMs(difference));
-  
+function changeTime(difference) {
+  const diff = convertMs(difference);
+  secondsEl.textContent = diff.seconds.toString().padStart(2, "0");
+  daysEl.textContent = diff.days.toString().padStart(2, "0");
+  hoursEl.textContent = diff.hours.toString().padStart(2, "0");
+  minutesEl.textContent = diff.minutes.toString().padStart(2, "0");
 }
 
 function btnOnClick(event) {
-    changeTime();
-  timerId = setInterval(changeTime, 1000);
+  changeTime(flp.selectedDates[0] - Date.now());
+  const timerId = setInterval(() => {
+    const currentDate = new Date();
+    const difference = flp.selectedDates[0] - currentDate;
+     if (difference <= 0) {
+    clearInterval(timerId);
+    return;
+    };
+    changeTime(difference)
+  },
+    1000);
   btnStart.disabled = true;
   input.disabled = true;
-
-
 };
